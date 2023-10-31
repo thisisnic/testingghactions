eb5006d96f3aeef24284a26d7cefe2ecf3c4a4be
thisisnic <thisisnic@users.noreply.github.com>
2023-10-31 08:54:55 +0000

    Style code (GHA)

--- R/convert_temps.R
+++ R/convert_temps.R
@@ -1,9 +1,7 @@
-fahrenheit_to_celsius <- function(f){
-  (f - 32)/1.8
+fahrenheit_to_celsius <- function(f) {
+  (f - 32) / 1.8
 }
 
-celsius_to_fahrenheit <- function(c){
-  (c*1.8) + 32
+celsius_to_fahrenheit <- function(c) {
+  (c * 1.8) + 32
 }
-
-
